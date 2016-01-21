003246c547ce092df9a6956ae9c151fa718d89db
Chad Whitacre <chad@zetaweb.com>
2016-01-21 07:37:32 -0500

    Take out some rename.py debugging

--- bin/rename.py
+++ bin/rename.py
@@ -11,11 +11,6 @@ import reporting
 
 OLD, NEW = sys.argv[1:3]
 
-
-'''
-    Assets:Escrow:Samurai                                           $     3.30
-'''
-
 chdir(reporting.root)
 for filename in reporting.list_datfiles():
     with open(filename, 'r') as fp:
@@ -23,8 +18,6 @@ for filename in reporting.list_datfiles():
     for tmpl in ("    {:<64}$", "    {:<63}-$", "    {}\n"):
         old = tmpl.format(OLD)
         new = tmpl.format(NEW)
-        print(old)
-        print(new)
         contents = contents.replace(old, new)
     with open(filename, 'w+') as fp:
         fp.write(contents)
