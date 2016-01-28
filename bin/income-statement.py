7f878101248ebd3d8ea0e5b6e36dd5f533aef6eb
Chad Whitacre <chad@zetaweb.com>
2016-01-28 18:01:02 -0500

    Factor out income_statement to share with escrow
    We want separate income and escrow statements

--- bin/income-statement.py
+++ bin/income-statement.py
@@ -3,42 +3,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import sys
 from os import path
-import calendar
 
 ourdir = path.realpath(path.dirname(__file__))
 sys.path.insert(0, ourdir)
 
 import reporting
 
-base = path.realpath(path.join(path.dirname(__file__), '..'))
-cmd = [ 'ledger'
-      , 'balance'
-      , '^Income'
-      , '^Expense'
-      , '--prepend-width=0' # this is here to satisfy ledger on Travis
-      , '--limit "not (payee =~ /^Retained Earnings$/)"'
-      , '--sort "account =~ /^Income.*/ ? 0 : '
-      ,        '(account =~ /^Expense.*/ ? 1 : 2))"'
-       ]
-cmd += sys.argv[1:]
-
-start, end = reporting.parse(sys.argv[1:])
-cmd += reporting.list_datfiles(start, end)
-
-print()
-print("INCOME STATEMENT".center(42))
-if start == end:
-    print("for {}, {}".format(calendar.month_name[int(end[1])], end[0]).center(42))
-elif start[0] == end[0]:
-    print("for {} through {}, {}".format( calendar.month_name[int(start[1])]
-                                        , calendar.month_name[int(end[1])]
-                                        , end[0]).center(42)
-                                         )
-else:
-    print("for {}, {} through {}, {}".format( calendar.month_name[int(start[1])]
-                                            , start[0]
-                                            , calendar.month_name[int(end[1])]
-                                            , end[0]
-                                             ).center(42))
-print()
-reporting.report(cmd)
+reporting.income_statement()
--- bin/reporting.py
+++ bin/reporting.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import argparse
+import calendar
 import subprocess
 import re
 import sys
@@ -94,6 +95,42 @@ def list_datfiles(start=None, end=None, root='.'):
         sys.exit('Error: {}-{} comes before {}-{}.'.format(*(end + start)))
 
     return ['-f ' + f for f in filtered]
+
+
+def income_statement():
+    """Produce an income statement for either operations or escrow.
+    """
+    cmd = [ 'ledger'
+          , 'balance'
+          , '^Income'
+          , '^Expense'
+          , '--prepend-width=0' # this is here to satisfy ledger on Travis
+          , '--limit "not (payee =~ /^Retained Earnings$/)"'
+          , '--sort "account =~ /^Income.*/ ? 0 : '
+          ,        '(account =~ /^Expense.*/ ? 1 : 2))"'
+           ]
+    cmd += sys.argv[1:]
+
+    start, end = parse(sys.argv[1:])
+    cmd += list_datfiles(start, end)
+
+    print()
+    print("INCOME STATEMENT".center(42))
+    if start == end:
+        print("for {}, {}".format(calendar.month_name[int(end[1])], end[0]).center(42))
+    elif start[0] == end[0]:
+        print("for {} through {}, {}".format( calendar.month_name[int(start[1])]
+                                            , calendar.month_name[int(end[1])]
+                                            , end[0]).center(42)
+                                             )
+    else:
+        print("for {}, {} through {}, {}".format( calendar.month_name[int(start[1])]
+                                                , start[0]
+                                                , calendar.month_name[int(end[1])]
+                                                , end[0]
+                                                 ).center(42))
+    print()
+    report(cmd)
 
 
 def in_root(func):
