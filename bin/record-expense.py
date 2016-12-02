f4bbe0b79be727b7134e2d90d4f78e785d86293c
Kwesi Aguillera <kaguillera@gmail.com>
2016-12-02 14:39:39 -0500

    Added validation for monetary amount.

--- bin/record-expense.py
+++ bin/record-expense.py
@@ -13,6 +13,7 @@ Expect : $ Expense Recorded
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 from datetime import datetime
+from decimal import InvalidOperation, Decimal as D
 import sys
 
 EXPENSE = """
@@ -30,7 +31,7 @@ if len(sys.argv) < 3:
     raise SystemExit("""
                         Usage: record-expense.py <payee> <amount> [date]
                         NB: Date must be in the format yyyy-mm-dd
-                     
+                        NB: Amount must be a valid monetary amount without the dollar sign 
                      """)
 
 today = datetime.now()
@@ -43,8 +44,6 @@ else:
 month = today.month
 year = today.year
 
-print (year) 
-
 if month < 6:
     folder = 'FY%s' %  (year)
 else:
@@ -53,6 +52,11 @@ else:
 payee = sys.argv[1]
 amount = sys.argv[2]
 
+
+try:
+    D(amount)
+except InvalidOperation:
+    raise SystemExit("Please enter a valid monetary value with out the dollar sign")
 
 dat_file = '%s/%d-%d.dat' % (folder, year, month)
 
@@ -63,6 +67,3 @@ with open(dat_file, 'a') as f:
     f.write(template.format(**dict(Date=date, Payee=sys.argv[1], amount=sys.argv[2])))
 
 print (template.format(**dict(Date=date, Payee=sys.argv[1], amount=sys.argv[2])))
-#month = datetime.now().strftime('%Y-%m')
-
-
