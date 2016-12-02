3fa90080d2f39623de62135709fb97ea2f56bee5
Kwesi Aguillera <kaguillera@gmail.com>
2016-12-02 13:36:17 -0500

    This initial srcipt. Almost no validation

---
+++ bin/record-expense.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+"""Add an expense to the .dat for the current month
+
+CMD    : $ record-expense.py github 14.95 2016-12-01
+Expect : $ Expense Recorded 
+
+# TODO 
+# 1. Validate Date and Amount
+# 2. Add functionality to record more types of expenses
+# 3. When 2 is implemented validate against declareation.dat
+
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from datetime import datetime
+import sys
+
+EXPENSE = """
+
+{Date}  {Payee}
+    Expense:Operations:SAS                          ${amount:10}                      
+    Asset:Operations:PNC                           -${amount:10} 
+{Date}  Current Activity
+    Equity:Current Activity                         ${amount:10}
+    Expenses:Operations:SAS                        -${amount:10}
+
+"""
+
+if len(sys.argv) < 3:
+    raise SystemExit("""
+                        Usage: record-expense.py <payee> <amount> [date]
+                        NB: Date must be in the format yyyy-mm-dd
+                     
+                     """)
+
+today = datetime.now()
+
+if len(sys.argv) == 3:
+    date = today.strftime('%Y-%m-%d')
+else: 
+    date= sys.argv[3]
+
+month = today.month
+year = today.year
+
+print (year) 
+
+if month < 6:
+    folder = 'FY%s' %  (year)
+else:
+    folder = 'FY%s' % (year+1)
+
+payee = sys.argv[1]
+amount = sys.argv[2]
+
+
+dat_file = '%s/%d-%d.dat' % (folder, year, month)
+
+template = EXPENSE 
+
+
+with open(dat_file, 'a') as f:
+    f.write(template.format(**dict(Date=date, Payee=sys.argv[1], amount=sys.argv[2])))
+
+print (template.format(**dict(Date=date, Payee=sys.argv[1], amount=sys.argv[2])))
+#month = datetime.now().strftime('%Y-%m')
+
+
